db1ae6465f06b51b16a9605763e94a0dae07d977
KirillFilatov <kirafill01@yandex.ru>
2022-09-01 18:53:11 +0300

    first commit

---
+++ scripts/idleEnemy.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+class_name Enemy
+var velocity:Vector2
+var speed = 9000
+var isMoving = false
+var navMesh:Navigation2D
+var path:PoolVector2Array
+var point = 0
+var health = 100
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func get_class():
+	return 'Enemy'
+func getDamage(damage):
+	health -= damage
+	if health < 0:
+		$AnimatedSprite.play("dead")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
